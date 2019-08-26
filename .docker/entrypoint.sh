ce41ef7b9b52eab7240869f22c52cf8eeff2cb94
Allan Lucio <luciocorreia.allan@gmail.com>
2019-08-26 17:47:47 -0300

    added migrate --seed on entrypoint.sh

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,7 +3,7 @@
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 composer install
 php artisan key:generate
-php artisan migrate
+php artisan migrate --seed
 chown -R www-data:1000 /var/www
 chmod -R 775 /var/www
 php-fpm
