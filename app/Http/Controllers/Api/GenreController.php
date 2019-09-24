610e74640cc00a8615843e916d62fb7edcf7525c
allan <luciocorreia.allan@gmail.com>
2019-09-24 18:46:59 -0300

    finished resource video and relationships

--- app/Http/Controllers/Api/GenreController.php
+++ app/Http/Controllers/Api/GenreController.php
@@ -5,13 +5,49 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Genre;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class GenreController extends BasicCrudController
 {
     private $rules = [
         "name" => "required|max:255",
-        "is_active" => "boolean"
+        "is_active" => "boolean",
+        "categories_id"=>"required|array|exists:categories,id"
     ];
+
+    public function store(Request $request)
+    {
+        $validated_data = $this->validate($request,$this->rulesStore());
+        $self = $this;
+        $obj = DB::transaction(function () use($request,$validated_data,$self){
+            $obj = $this->model()::create($validated_data);
+            $self->handleRelations($obj,$request);
+            return $obj;
+        });
+
+        $obj->refresh();
+
+        return $obj;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated_data = $this->validate($request,$this->rulesUpdate());
+        $obj=$this->findOrFail($id);
+        $self = $this;
+        $obj = DB::transaction(function () use($request,$validated_data,$obj,$self){
+            $obj->update($validated_data);
+            $self->handleRelations($obj,$request);
+            return $obj;
+        });
+
+        return $obj;
+    }
+
+    protected function handleRelations($video, Request $request){
+        $video->categories()->sync($request->get("categories_id"));
+
+    }
 
     protected function model(){
         return Genre::class;
